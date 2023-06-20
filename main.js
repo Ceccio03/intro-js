6678357e53e5d1467f6405c728ba13372e49e1e8
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-06-20 10:40:13 +0200

    commit

---
+++ main.js
@@ -0,0 +1,76 @@
+console.log('hello world!')
+
+// Tipi di base
+
+// 'fuygub'    // stringa
+
+// 34 56.6 -23     // numbers
+
+// true false  // boolean
+
+// Tipi strani:
+
+// NaN Not a Number
+
+// variabili con let--------------------------------------------------------------------
+// let pippo = 5;
+
+// console.log(typeof pippo);
+
+// pippo = 'viva il caffè!';
+
+// console.log(typeof pippo);
+
+// console.log(pippo);
+
+// variabili con const-----------------------------------------------------------------
+// const pluto = 12;
+
+// pluto = 23;
+
+// console.log(pluto);
+
+// variabili con var-------------------------------------------------------------------
+// var paperino = true;
+
+// console.log(paperino);
+
+// paperino = -123;
+
+// console.log(paperino);
+
+// differenza con let
+// topolino = 5;
+
+// let topolino;
+
+// minnie = 5;
+
+// var minnie;
+
+// console.log(minnie);
+
+let qui = (5 + 9);
+let quo = (5 * 9);
+let qua = (5 / 9);
+let paperone = 5 ** 3;  // elevamento a potenza
+let basettoni = 6 % 5;  // modulo
+console.log(qui);
+console.log(quo);
+console.log(qua);
+console.log(paperone);
+console.log(basettoni);
+
+let gambaDiLegno = 8 % 2;
+console.log(gambaDiLegno);
+
+let ilNumeroPiuPiccolo = -Infinity;
+let ilNumeroPiuGrande = Infinity;
+
+const paperoga = Math.sqrt(16); // libreria Math
+console.log(paperoga);
+
+const rockerduck = 'pippo' / 3;
+console.log(rockerduck);
+
+// String
