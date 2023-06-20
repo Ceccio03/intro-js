6b08553c99085ebec71bb7260f9886b48be3cda1
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-06-20 12:28:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,11 @@ console.log('hello world!')
 
 // NaN Not a Number
 
-// variabili con let--------------------------------------------------------------------
+// undefined
+
+// null
+
+// variabili con let------------------------------------------------------------------------
 // let pippo = 5;
 
 // console.log(typeof pippo);
@@ -23,14 +27,14 @@ console.log('hello world!')
 
 // console.log(pippo);
 
-// variabili con const----------------------------------------------------------------- 
+// variabili con const----------------------------------------------------------------------
 // const pluto = 12;
 
 // pluto = 23;
 
 // console.log(pluto);
 
-// variabili con var-------------------------------------------------------------------
+// variabili con var-------------------------------------------------------------------------
 // var paperino = true;
 
 // console.log(paperino);
@@ -50,27 +54,115 @@ console.log('hello world!')
 
 // console.log(minnie);
 
-let qui = (5 + 9);
-let quo = (5 * 9);
-let qua = (5 / 9);
-let paperone = 5 ** 3;  // elevamento a potenza
-let basettoni = 6 % 5;  // modulo
-console.log(qui);
-console.log(quo);
-console.log(qua);
-console.log(paperone);
-console.log(basettoni);
+// let qui = (5 + 9);
+// let quo = (5 * 9);
+// let qua = (5 / 9);
+// let paperone = 5 ** 3;  // elevamento a potenza
+// let basettoni = 6 % 5;  // modulo
+// console.log(qui);
+// console.log(quo);
+// console.log(qua);
+// console.log(paperone);
+// console.log(basettoni);
 
-let gambaDiLegno = 8 % 2;
-console.log(gambaDiLegno);
+// let gambaDiLegno = 8 % 2;
+// console.log(gambaDiLegno);
 
-let ilNumeroPiuPiccolo = -Infinity;
-let ilNumeroPiuGrande = Infinity;
+// let ilNumeroPiuPiccolo = -Infinity;
+// let ilNumeroPiuGrande = Infinity;
 
-const paperoga = Math.sqrt(16); // libreria Math
-console.log(paperoga);
+// const paperoga = Math.sqrt(16); // libreria Math
+// console.log(paperoga);
 
-const rockerduck = 'pippo' / 3;
-console.log(rockerduck);
+// const rockerduck = 'pippo' / 3;
+// console.log(rockerduck);
 
-// String
+// Boolean------------------------------------------------------------------------------------
+// const etabeta = true;
+// console.log(etabeta);
+
+// // ! (not)    !true => false;
+// //          !false => true;
+
+// console.log(!etabeta);
+
+// // && (and)     true && true => true
+// //              true && false => false
+// //              false && true => false
+// //              false && false => false
+
+// const gastone = false;
+// console.log(etabeta && gastone);
+// console.log(etabeta && !gastone);
+
+// // || (or)      true || true => true
+// //              true || false => true
+// //              false || true => true
+// //              false || false => false
+
+// console.log(etabeta || gastone);
+// console.log(!etabeta || gastone);
+// console.log((etabeta || gastone) && etabeta);
+
+// String----------------------------------------------------------------------------------------
+// const orazio = 'io adoro javascript';
+// console.log(orazio[0]);
+// console.log(orazio[9]);
+
+// const clarabella = 'perché non sono sano di mente';
+// console.log(orazio + ' ' + clarabella);
+
+// const aladin = 27;
+// console.log(orazio + 27);
+// console.log(clarabella.length);
+// console.log(clarabella.toUpperCase());
+
+// undefined and null-----------------------------------------------------------------------------
+// const macchiaNera = 5;
+// // console.log(paperinik);
+
+// let paperinik;
+// console.log(paperinik); // undefined => scatola vuota
+// paperinik = 12;
+// console.log(paperinik);
+
+// paperinik = undefined;  // nooooooooooooo;
+// console.log(paperinik);
+// paperinik = null;
+// console.log(paperinik);
+
+// operatori di confronto-----------------------------------------------------------------------
+// const nonnaPapera = 12;
+
+// const ciccio = 24;
+
+// const joseCarioca = '12';
+
+// // > (maggiore)
+// // >= (maggiore o uguale)
+// console.log(ciccio >= nonnaPapera);
+
+// // < (minore)
+// // <= (minore o uguale)
+// console.log(ciccio < nonnaPapera);
+
+// // != (diverso) (noooooooooo)
+// // !== (diverso)
+// console.log(ciccio !== nonnaPapera);
+
+// // == (uguale con cast)
+// // === (uguale stretto)
+// console.log(nonnaPapera == joseCarioca);    // mai usare questo qui!!!
+// console.log(nonnaPapera === joseCarioca);
+
+// corto circuito degli operatori-----------------------------------------------------------------
+// let biancaNeve;
+// let ariel = 'ciao';
+// let cenerentola = biancaNeve || ariel;
+// console.log(cenerentola);
+
+// alert e prompt----------------------------------------------------------------------------------
+alert('ciao a tutti');
+
+const elsa = prompt('come ti chiami?');
+console.log(elsa);
