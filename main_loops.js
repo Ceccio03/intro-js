b65bc80ba98c5216d4b69f07b12eb45539d7bd23
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-06-21 12:56:59 +0200

    Update main_loops.js

--- main_loops.js
+++ main_loops.js
@@ -82,6 +82,7 @@ console.log('looooooops');
 //     console.log(char);
 // }
 
+// ESERCIZIO 1
 // let string = '#'
 
 // for (let index = 0; index < 7; index++) {
@@ -96,14 +97,31 @@ console.log('looooooops');
 //     string2 = string2 + '#';
 // }
 
-for (let index = 1; index <= 100; index++) {
-    if (index % 3 === 0 && index % 5 === 0) {
-        console.log('FizzBuzz');
-    } else if (index % 3 === 0) {
-        console.log('Fizz');
-    } else if (index % 5 === 0) {
-        console.log('Buzz');
-    } else {
-        console.log(index);
+// ESERCIZIO 2
+// for (let index = 1; index <= 100; index++) {
+//     if (index % 3 === 0 && index % 5 === 0) {
+//         console.log('FizzBuzz');
+//     } else if (index % 3 === 0) {
+//         console.log('Fizz');
+//     } else if (index % 5 === 0) {
+//         console.log('Buzz');
+//     } else {
+//         console.log(index);
+//     }
+// }
+
+// CICLI NESTATI-----------------------------------------------------------------------------
+// for (let i = 0; i < 10; i++) {
+//     for (let j = 0; j < 10; j++) {
+//         console.log('riga: ' + i + ', colonna: ' + j);
+//     }
+// }
+
+// ESERCIZIO 3
+for (let i = 0; i < 8; i++) {
+    for (let j = 0; j< 10; j++) {
+        
+        
     }
+    
 }
