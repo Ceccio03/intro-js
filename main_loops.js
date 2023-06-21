2ac73308153c94298354350b6cb0a6e3decde137
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-06-21 09:48:36 +0200

    loops

---
+++ main_loops.js
@@ -0,0 +1,10 @@
+console.log('looooooops');
+
+// CICLO WHILE
+let indice = 0;
+
+while (indice < 10) {
+    console.log('sono un\'istruzione che deve essere ripetuta');
+
+    indice = indice + 1;
+}
