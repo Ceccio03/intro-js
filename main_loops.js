662b70cc69a20f6d3ae7e7f3972fd1ba2e4f6dd1
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-06-21 12:36:02 +0200

    Update main_loops.js

--- main_loops.js
+++ main_loops.js
@@ -89,19 +89,21 @@ console.log('looooooops');
 //     string = string + '#';
 // }
 
-let string2 = '#';
+// let string2 = '#';
 
-while (string2.length < 8) {
-    console.log(string2);
-    string2 = string2 + '#';
-}
-
-// for (let index = 100; index >= 1; index++) {
-//     if (index % 3 === 0) {
-//         console.log("Fizz");
-//     } else if (index % 5 === 0) {
-//         console.log("Buzz");
-//     } else if (index % 3 === 0 && index % 5 === 0) {
-//         console.log("FizzBuzz")
-//     }
+// while (string2.length < 8) {
+//     console.log(string2);
+//     string2 = string2 + '#';
 // }
+
+for (let index = 1; index <= 100; index++) {
+    if (index % 3 === 0 && index % 5 === 0) {
+        console.log('FizzBuzz');
+    } else if (index % 3 === 0) {
+        console.log('Fizz');
+    } else if (index % 5 === 0) {
+        console.log('Buzz');
+    } else {
+        console.log(index);
+    }
+}
