96309c5489b9526890e3b62543e39ffb39918659
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-06-22 11:26:34 +0200

    Update main_functions.js

--- main_functions.js
+++ main_functions.js
@@ -1,62 +1,91 @@
 console.log('functions');
 
-function square(specialNumber) {
-    const result = specialNumber * specialNumber;
+// function square(specialNumber) {
+//     const result = specialNumber * specialNumber;
     
+//     return result;
+// }
+
+// function logUppercase(stringToLog) {
+//     const uppercaseString = stringToLog.toUpperCase();
+
+//     console.log(uppercaseString);
+// }
+
+// function gentleAllert() {
+//     alert('caro amico, non vorrei farmi gli affari tuoi, ma sarà il caso che ti metti a studiare');
+// }
+
+// function logMultipleTimes(stringToLog, times) {
+//     for (let i = 0; i < times; i++) {
+//         console.log(stringToLog);
+//     }
+// }
+
+// function isEven(number) {
+//     if (number % 2 === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// function pow(base, exponent) {
+//     const result = base ** exponent;
+
+//     return result;
+// }
+
+// function lastCharUppercase(selectedString) {
+//     const char = selectedString[selectedString.length - 1];
+//     const charUpperCase = char.toUpperCase();
+
+//     return charUpperCase;
+// }
+
+// const pippo = square(16);
+// console.log(pippo);
+
+// logUppercase('viva le funzioni');
+
+// gentleAllert();
+
+// logMultipleTimes('pizza', 100);
+
+// const pluto = isEven(27);
+// console.log(pluto);
+
+// const paperino = pow(2, 10);
+// console.log(paperino);
+
+// const gastone = lastCharUppercase('genova');
+// console.log(gastone);
+
+// SINTASSI ALTERNATIVE-------------------------------------------------------------
+function square(number) {
+    // const result = number * number;
+    // return result;
+    const result = number * number;
+
     return result;
 }
 
-function logUppercase(stringToLog) {
-    const uppercaseString = stringToLog.toUpperCase();
+console.log(square(3));
 
-    console.log(uppercaseString);
-}
 
-function gentleAllert() {
-    alert('caro amico, non vorrei farmi gli affari tuoi, ma sarà il caso che ti metti a studiare');
-}
-
-function logMultipleTimes(stringToLog, times) {
-    for (let i = 0; i < times; i++) {
-        console.log(stringToLog);
-    }
-}
-
-function isEven(number) {
-    if (number % 2 === 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function pow(base, exponent) {
-    const result = base ** exponent;
+const square2 = function(number) {
+    const result = number * number;
 
     return result;
-}
+}   // anonimous function
 
-function lastCharUppercase(selectedString) {
-    const char = selectedString[selectedString.length - 1];
-    const charUpperCase = char.toUpperCase();
+console.log(square2(10));
 
-    return charUpperCase;
-}
 
-const pippo = square(16);
-console.log(pippo);
+const square3 = (number) => {
+    const result = number * number;
 
-logUppercase('viva le funzioni');
+    return result;
+}   // anonimous function
 
-gentleAllert();
-
-logMultipleTimes('pizza', 100);
-
-const pluto = isEven(27);
-console.log(pluto);
-
-const paperino = pow(2, 10);
-console.log(paperino);
-
-const gastone = lastCharUppercase('genova');
-console.log(gastone);
+console.log(square2(4));
