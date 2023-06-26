550975af98bb7cb85f56217dcc63bf5e8686ccdb
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-06-26 10:40:23 +0200

    Update main_object.js

--- main_object.js
+++ main_object.js
@@ -1,1 +1,13 @@
-console.log('oggetiiiii!');
+console.log('oggettiiiii!');
+
+const studente = {nome: 'Damiano', anno: 1993, sposato: false};
+
+console.log(studente);
+console.log(studente.nome);
+console.log(studente['anno']);
+
+studente.indirizzo = 'taggia';
+console.log(studente);
+console.log(studente.conto);
+console.log(Object.keys(studente));
+console.log(Object.values(studente));
