a7a31d8adafc9a7a2c6d8e6836e78924e4a4dd01
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-06-26 10:54:11 +0200

    Update main_object.js

--- main_object.js
+++ main_object.js
@@ -9,5 +9,31 @@ console.log(studente['anno']);
 studente.indirizzo = 'taggia';
 console.log(studente);
 console.log(studente.conto);
-console.log(Object.keys(studente));
-console.log(Object.values(studente));
+console.log(Object.keys(studente)); // tutte le etichette
+console.log(Object.values(studente));   // tutti i valori
+
+for (const key in studente) {
+    const value = studente[key];
+
+    console.log(key, value);
+}
+
+// for (let i = 0; i < Object.keys(studente).length; i++) {
+//     const key = Object.keys(studente)[i];
+//     const value = studente[key];
+// }
+
+const studente2 = {
+    name: 'Manuela',
+    yob: 1988,
+    isMarried: false,
+    address: 'Genova',
+    marks: [8, 9, 7, 8, 10],
+    dog: {
+        name: 'Nala',
+        breed: 'lupo cecoslovacco',
+        yob: 2017
+    }
+};
+
+console.log(studente2);
