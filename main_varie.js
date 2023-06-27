d91d7fc98ba180db5d915325b0eb2020325d8186
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-06-27 09:44:43 +0200

    Update main_varie.js

--- main_varie.js
+++ main_varie.js
@@ -1,1 +1,29 @@
 console.log('looooog');
+
+// SCOPE--------------------------------------------------------
+
+// scope globale è dichiarato fuori da qualsiasi graffa
+const pippo = 10;   // scope globale
+
+if (pippo < 20) {
+    pippo = pippo + 3;
+
+    let pluto = 4;
+}
+
+// pluto = pluto + 1;
+
+// console.log(pippo);
+console.log(pluto);
+
+// SCOPE NESTATI---------------------------------------------
+function createEvenArray(max) {
+    const tempArray = [];
+
+    for (let i = 0; i <= max; i++) {
+        if (i % 2 === 0) {
+            tempArray.push(i);
+        }
+    }
+    return tempArray;
+}
