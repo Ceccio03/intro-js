11ec47b72c199bfb5bda8a68ba905b74fc074af0
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-06-28 11:33:59 +0200

    Update h-o-functions.js

--- h-o-functions.js
+++ h-o-functions.js
@@ -143,59 +143,106 @@ console.log('order');
 // console.log(testArray3.filter(isIndexEven));
 // console.log(testArray3.filter((element, index) => index % 2 === 0));
 
-function sumAll(array) {
-    let accumulator = 0;
+// function sumAll(array) {
+//     let accumulator = 0;
 
-    for (let i = 0; i < array.length; i++) {
-        const current = array[i];
+//     for (let i = 0; i < array.length; i++) {
+//         const current = array[i];
         
-        accumulator += current;
-    }
-    return accumulator;
-}
-console.log(sumAll([23, 4, 4, 6, 3]));
+//         accumulator += current;
+//     }
+//     return accumulator;
+// }
+// console.log(sumAll([23, 4, 4, 6, 3]));
 
-function reduce(array, reduceFunc, startingValue) {
-    let accumulator = startingValue;
+// function reduce(array, reduceFunc, startingValue) {
+//     let accumulator = startingValue;
 
-    for (let i = 0; i < array.length; i++) {
-        const current = array[i];
+//     for (let i = 0; i < array.length; i++) {
+//         const current = array[i];
         
-        accumulator = reduceFunc(accumulator, current);
-    }
-    return accumulator;
+//         accumulator = reduceFunc(accumulator, current);
+//     }
+//     return accumulator;
+// }
+
+// function sum(accumulator, current) {
+//     console.log(accumulator);
+//     console.log(current);
+
+//     const newAccumulator = accumulator + current;
+
+//     return newAccumulator;
+// }
+// console.log(reduce([23, 4, 4, 6, 3], sum, 0));
+
+
+// const testArray4 = [4, 3, 2, 1];
+
+// function multiply(acc, curr) {
+//     // const newAcc = acc * curr;
+
+//     // return newAcc;
+//     return acc * curr;
+// }
+// console.log(testArray4.reduce(multiply, 1));
+// console.log(testArray4.reduce((acc, curr) => acc * curr, 1));
+
+
+// const multiplyLambda = (acc, curr) => {
+//     const newAcc = acc * curr;
+
+//     return newAcc;
+// }
+
+// const multiplyLambda2 = (acc, curr) => acc * curr;
+
+// console.log(testArray4.reduce((acc, curr) => {
+//     const newAcc = acc * curr;
+
+//     return newAcc;
+// }, 1));
+
+// function sumOnlyOddIndex(acc, curr, index, originalArray) {
+//     if (index % 2 !== 0) {
+//         const newAcc = acc + curr;
+
+//         return newAcc;
+//     } else {
+//         return acc;
+//     }
+// }
+// console.log(testArray4.reduce(sumOnlyOddIndex, 0));
+
+// function sumAll2(array) {
+//     let accumulator = array[0];
+
+//     for (let i = 1; i < array.length; i++) {
+//         const current = array[i];
+        
+//         accumulator += current;
+//     }
+//     return accumulator;
+// }
+// console.log(sumAll2([2, 3, 4]));
+// console.log(sumAll([2, 3, 4]));
+
+// FUNZIONI CHE RESTITUISCONO FUNZIONI
+
+function multiplyBy2(number) {
+    const result = number * 2;
+
+    return result;
 }
 
-function sum(accumulator, current) {
-    const newAccumulator = accumulator + current;
+function multiplyBy4(number) {
+    const result = number * 4;
 
-    return newAccumulator;
+    return result;
 }
-console.log(reduce([23, 4, 4, 6, 3], sum, 0));
+console.log(multiplyBy2(2));
+console.log(multiplyBy4(3));
 
-
-const testArray4 = [4, 3, 2, 1];
-
-function multiply(acc, curr) {
-    // const newAcc = acc * curr;
-
-    // return newAcc;
-    return acc * curr;
+function multiplyGenerator(multiplicator) {
+    
 }
-console.log(testArray4.reduce(multiply, 1));
-console.log(testArray4.reduce((acc, curr) => acc * curr, 1));
-
-
-const multiplyLambda = (acc, curr) => {
-    const newAcc = acc * curr;
-
-    return newAcc;
-}
-
-const multiplyLambda2 = (acc, curr) => acc * curr;
-
-console.log(testArray4.reduce((acc, curr) => {
-    const newAcc = acc * curr;
-
-    return newAcc;
-}, 1));
