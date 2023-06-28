a89b19d4912c5643cbd9f4552933e6aba3d9d20f
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-06-28 09:13:38 +0200

    Update h-o-functions.js

--- h-o-functions.js
+++ h-o-functions.js
@@ -1,1 +1,33 @@
 console.log('order');
+
+// HIGH ORDER FUNCTIONS-----------------------------------
+// MAP
+function addOneToArray(arrayOfNumbers) {
+    let tempArray = [];
+
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+        const element = arrayOfNumbers[i];
+        const newElement = element + 1;
+        
+        tempArray.push(newElement);
+    }
+    return tempArray;
+}
+console.log(addOneToArray([3, 7, 0]));
+
+function divideByTwoArray(arrayOfNumbers) {
+    let tempArray = [];
+
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+        const element = arrayOfNumbers[i];
+        const newElement = element / 2;
+        
+        tempArray.push(newElement);
+    }
+    return tempArray;
+}
+console.log(divideByTwoArray([3, 7, 0]));
+
+function map(array, transFunc) {
+    
+}
