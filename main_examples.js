fcdf19f307860388f523845ee9c1a4e7bd72ed0f
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-03 09:34:33 +0200

    Update main_examples.js

--- main_examples.js
+++ main_examples.js
@@ -5,6 +5,11 @@ const paperino = true;
 const qui = undefined;
 const quo = null;
 const qua = NaN;
+const clarabella = {
+    name: 'isabella',
+    yob: 1996
+};
+const orazio = ['ciao', 'hello'];
 
 console.log(typeof pippo);
 console.log(typeof pluto);
@@ -12,3 +17,34 @@ console.log(typeof paperino);
 console.log(typeof qui);
 console.log(typeof quo);
 console.log(typeof qua);
+console.log(qua === NaN);
+console.log(isNaN(qua));
+console.log(typeof clarabella);
+console.log(typeof orazio);
+
+function range(start, end, step = 1, reversed = false) {
+    const tempArray = [];
+
+    if (reversed) {
+    
+        if (start < end) {
+            return tempArray;
+        }
+
+        for (let i = start; i >= end; i -= step) {
+            tempArray.push(i);
+        }
+        return tempArray;
+    } else {
+        if (end < start) {
+            return tempArray;
+        }
+        for (let i = start; i <= end; i += step) {
+            tempArray.push(i);
+        }
+    }
+    return tempArray;
+}
+console.log(range(-5, 50));
+console.log(range(0, 100, 2));
+console.log(range(-5, 50));
