8f8d695eb58c01e4349072d7d849bedb95909f0b
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-03 09:06:28 +0200

    new file

---
+++ main_examples.js
@@ -0,0 +1,14 @@
+// TYPEOF
+const pippo = 12;
+const pluto = 'ciao mondo';
+const paperino = true;
+const qui = undefined;
+const quo = null;
+const qua = NaN;
+
+console.log(typeof pippo);
+console.log(typeof pluto);
+console.log(typeof paperino);
+console.log(typeof qui);
+console.log(typeof quo);
+console.log(typeof qua);
